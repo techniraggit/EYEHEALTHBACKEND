251494ca685ec0112f8651c96365883586b536c3
Rajat <rajat@niraginfotech.com>
2024-05-28 18:26:57 +0530

    fix

--- api/views/subscription.py
+++ api/views/subscription.py
@@ -11,7 +11,7 @@ class IsActivePlan(UserMixin):
     def get(self, obj):
         plan_obj = UserSubscription.objects.filter(user=obj.user).order_by("created_on")
         is_active_plan = plan_obj.first().is_active if plan_obj.first() else False
-        plan_id = plan_obj.first().id if plan_obj.first() else 0
+        plan_id = plan_obj.first().plan.id if plan_obj.first() else 0
         return api_response(True, 200, is_active_plan=is_active_plan, plan_id=plan_id)
 
 
