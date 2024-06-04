572f6a8df0891a4d17b4a908b9ad28496f18c12c
Rajat <rajat@niraginfotech.com>
2024-06-04 19:02:40 +0530

    offers workin

--- AdminHub/views/offers.py
+++ AdminHub/views/offers.py
@@ -7,14 +7,15 @@ from django.contrib import messages
 
 class OffersView(AdminLoginView):
     def get(self, request):
-        offers = Offers.objects.all().order_by("created_on")
+        offers = Offers.objects.all().order_by("-created_on")
         context = dict(
             offers=offers,
             is_offer=True,
         )
         return render(request, "offers/offers.html", context)
 
-
+from django.http import JsonResponse
+from django.urls import reverse
 class AddOffersView(AdminLoginView):
     def get(self, request):
         offer_form = OffersForm()
@@ -25,20 +26,32 @@ class AddOffersView(AdminLoginView):
         return render(request, "offers/add_offer.html", context)
 
     def post(self, request):
-        offer_form = OffersForm(request.POST, request.FILES)
-        if offer_form.is_valid():
-            offer_obj = offer_form.save(commit=False)
-            offer_obj.created_by = request.user
-            offer_obj.updated_by = request.user
-            offer_obj.save()
-            messages.success(request, "Offer added successfully")
-            return redirect("offers_view")
-        context = dict(
-            offer_form=offer_form,
-            is_offer=True,
-        )
-        messages.error(request, offer_form.errors)
-        return render(request, "offers/add_offer.html", context)
+        try:
+            print("here>>>>>>>>>>>>>>>>>>>>>>>>")
+            offer_form = OffersForm(request.POST, request.FILES)
+            if offer_form.is_valid():
+                offer_obj = offer_form.save(commit=False)
+                offer_obj.created_by = request.user
+                offer_obj.updated_by = request.user
+                offer_obj.save()
+                # messages.success(request, "Offer added successfully")
+                response = {
+                    "success": True,
+                    "message": "Offer added successfully",
+                    "redirect_url": reverse("offers_view"),
+                }
+                return JsonResponse(response, status=200)
+
+            errors = offer_form.errors.as_json()
+            response = {
+                "success": False,
+                "errors": errors,
+                "redirect_url": reverse("add_offer_view"),
+            }
+            return JsonResponse(response, status=400)
+        except Exception as e:
+            print(e)
+            return JsonResponse({"success": False, "message":str(e)}, status=400)
 
 
 class EditOfferView(AdminLoginView):
@@ -77,7 +90,7 @@ class EditOfferView(AdminLoginView):
 
 class RedeemedOffersView(AdminLoginView):
     def get(self, request):
-        redeemed_offers = UserRedeemedOffers.objects.all().order_by("created_on")
+        redeemed_offers = UserRedeemedOffers.objects.all().order_by("-created_on")
         context = dict(
             redeemed_offers=redeemed_offers,
             is_redeemed_offers=True,
