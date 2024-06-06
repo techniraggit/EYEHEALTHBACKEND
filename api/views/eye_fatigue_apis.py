1d6d151f0ea5fa386993afa2316d47dd2000ac84
Rajat <rajat@niraginfotech.com>
2024-06-06 18:01:33 +0530

    testing

--- api/views/eye_fatigue_apis.py
+++ api/views/eye_fatigue_apis.py
@@ -60,6 +60,8 @@ def take_user_selfie(token, data):
 
 class TakeUserSelfie(UserMixin):
     def post(self, request):
+        logger.info(str(request.FILES))
+        logger.info(str(request.data))
         response = take_user_selfie(request.headers.get("Customer-Access-Token"), request.FILES)
         try:
             return Response(response.json(), response.status_code)
