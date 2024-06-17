41f5b05c5f66b459369551c39f2d121905f1e2a8
Rajat <rajat@niraginfotech.com>
2024-06-17 15:02:39 +0530

    add pagination in Notification

--- admin_panel/views/notifications.py
+++ admin_panel/views/notifications.py
@@ -1,6 +1,7 @@
 from .base import AdminLoginView
 from django.shortcuts import render
 from api.models.notifications import PushNotification
+from django.core.paginator import Paginator
 
 
 
@@ -8,8 +9,11 @@ class NotificationView(AdminLoginView):
     records_per_page = 10
     def get(self, request):
         notifications = PushNotification.objects.all()
+        paginator = Paginator(notifications, 10)
+        page_number = request.GET.get("page")
+        paginated_notifications = paginator.get_page(page_number)
         context = dict(
-            notifications = notifications,
+            notifications = paginated_notifications,
             is_notification = True,
         )
         return render(request, "notification/notifications.html", context)
