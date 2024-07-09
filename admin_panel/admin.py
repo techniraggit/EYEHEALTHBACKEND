96a940238660af0cf275a9d906864f77796014ca
Rajat <rajat@niraginfotech.com>
2024-07-09 13:26:34 +0530

    fix admin

--- admin_panel/admin.py
+++ admin_panel/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Credentials
 
 # Register your models here.
-admin.register(Credentials)
+@admin.register(Credentials)
 class CredentialsAdmin(admin.ModelAdmin):
     list_display = ["name",  "created_on", "updated_on"]
     search_fields = ["name"]
