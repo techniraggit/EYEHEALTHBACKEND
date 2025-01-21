33380685756c246f877efa665158a9ae1d516858
Rajat <rajat@niraginfotech.com>
2025-01-21 10:56:10 +0530

    fix carousel status

--- admin_panel/forms/carousels.py
+++ admin_panel/forms/carousels.py
@@ -5,3 +5,6 @@ class CarouselModelForm(forms.ModelForm):
     class Meta:
         model = CarouselModel
         fields = "__all__"
+
+    def clean_is_active(self):
+        return self.data.get("status") == "active"
