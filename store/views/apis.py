e111ad6ab98a54d77d238e601b368788abe99a09
Rajat <rajat@niraginfotech.com>
2025-01-30 16:09:07 +0530

    fix map response

--- store/views/apis.py
+++ store/views/apis.py
@@ -125,8 +125,16 @@ class NearbyStoresMapView(UserMixin):
                 "latitude": store.latitude,
                 "longitude": store.longitude,
                 "address": store.full_address(),
-                "image": store.images.all().first().image.url if store.images.all() else "",
-                "timing": (store.store_availability.all().first().start_working_hr, store.store_availability.all().first().end_working_hr),
+                "images": [
+                    f"{request.build_absolute_uri(i.image.url)}"
+                    for i in store.images.all()
+                ],
+                "opening_time": store.store_availability.first().start_working_hr.strftime(
+                    "%I:%M %p"
+                ),
+                "closing_time": store.store_availability.first().end_working_hr.strftime(
+                    "%I:%M %p"
+                ),
                 "rating": store.get_average_rating(),
                 "distance_km": round(store.distance.km, 2),
             }
